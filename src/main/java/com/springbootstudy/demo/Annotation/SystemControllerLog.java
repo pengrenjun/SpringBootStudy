07ffef96187358bf4291cda5a38eca122774270d
pengrj <pengrj@emrubik.com>
2018-08-29 16:42:23 +0800

    Annotation注解开发

---
+++ src/main/java/com/springbootstudy/demo/Annotation/SystemControllerLog.java
@@ -0,0 +1,15 @@
+package com.springbootstudy.demo.Annotation;
+
+import java.lang.annotation.*;
+
+/**
+ *自定义注解 拦截Controller
+ */
+
+@Target({ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SystemControllerLog {
+
+    String description()  default "";
+}
